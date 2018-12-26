df3998234a64a5225b3736b296064db972c66f44
Matthew C <matthew.j.carpenter1@gmail.com>
2018-12-26 16:29:36 -0500

    Updates

---
+++ precache-manifest.60f73e69d9c69a30e680eff8a9012466.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2d4588d46b08cb05323",
+    "url": "/React-Recipe/static/css/main.dec24cfc.chunk.css"
+  },
+  {
+    "revision": "f2d4588d46b08cb05323",
+    "url": "/React-Recipe/static/js/main.f2d4588d.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/React-Recipe/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "4f0c99040993c176ebb1",
+    "url": "/React-Recipe/static/js/runtime~main.4f0c9904.js"
+  },
+  {
+    "revision": "070e20770b09e9a030b06985d008f278",
+    "url": "/React-Recipe/index.html"
+  }
+];
